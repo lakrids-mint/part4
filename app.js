4ad07bd670d33db30160362ce3b255079b744eb0
lakrids-mint <mettepernille@hotmail.com>
2019-07-13 22:27:55 +0200

    implemented user creation and cross refs

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 const api = supertest(app);
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 
@@ -23,6 +25,7 @@ app.use(bodyParser.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,13 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response, next) => {
   try {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("user", {
+      username: 1,
+      name: 1
+    });
     response.json(blogs.map(blog => blog.toJSON()));
   } catch (e) {
     next(e);
@@ -49,14 +53,19 @@ blogsRouter.put("/:id", async (request, response, next) => {
 
 blogsRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   });
   try {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     response.json(savedBlog.toJSON());
   } catch (e) {
     next(e);
